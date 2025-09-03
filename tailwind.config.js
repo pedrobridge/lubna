112c8ac30dfe697070c33d5a34ec79b885e856b9
pedrobridge <pedrorubiraneto@gmail.com>
2025-09-03 11:40:57 +0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        sand: "#C2B280",
+        terracotta: "#E2725B",
+        beige: "#F5F5DC"
+      }
+    }
+  },
+  plugins: [],
+}
